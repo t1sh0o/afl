701eeb9201fbd3d35854f6b314ec8e4e6fdb4979
Tisho <tishoj@gmail.com>
2014-12-17 00:02:27 +0200

    fix filters issue

--- app/controllers/PlayersController.php
+++ app/controllers/PlayersController.php
@@ -8,7 +8,8 @@ class PlayersController extends \BaseController {
 	{
 		$this->player = $player;
 
-		$this->beforeFilter('auth|admin');
+		$this->beforeFilter('auth');
+		$this->beforeFilter('admin');
 	}
 
 	/**
